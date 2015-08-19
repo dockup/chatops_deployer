5b0b7f85d633637dacc0b6b2487398bde0fd55c2
Emil Soman <emil.soman@gmail.com>
2015-08-19 21:04:20 +0530

    Update default workspace to /var/www

--- lib/sinatra_deployer/deploy_job.rb
+++ lib/sinatra_deployer/deploy_job.rb
@@ -6,7 +6,7 @@ module SinatraDeployer
   class DeployJob
     include SuckerPunch::Job
 
-    WORKSPACE = ENV['DEPLOYER_WORKSPACE'] || '/etc/nginx/sites-enabled'
+    WORKSPACE = ENV['DEPLOYER_WORKSPACE'] || '/var/www'
     DEPLOYER_HOST = ENV['DEPLOYER_HOST'] || '127.0.0.1.xip.io'
     NGINX_SITES_ENABLED_DIR = ENV['NGINX_SITES_ENABLED_DIR'] || '/etc/nginx/sites-enabled'
 
