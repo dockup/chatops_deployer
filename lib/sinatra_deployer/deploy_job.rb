4d67c09d42c7eb8085fe7251cbb204d9d6290f5b
Emil Soman <emil.soman@gmail.com>
2015-08-19 21:09:43 +0530

    Fix --rm flag for docker run

--- lib/sinatra_deployer/deploy_job.rb
+++ lib/sinatra_deployer/deploy_job.rb
@@ -42,7 +42,7 @@ module SinatraDeployer
     def dockerup
       puts "Running docker container #{@deployment_alias}"
       system("docker build -t #{@deployment_alias} .") &&
-        system("docker run -rm -P -d --name=#{@deployment_alias} #{@deployment_alias}")
+        system("docker run --rm=true -P -d --name=#{@deployment_alias} #{@deployment_alias}")
     end
 
     def add_nginx_config
