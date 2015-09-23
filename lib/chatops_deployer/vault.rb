4dbcad2047efc72b2dec48ebe897adfb5180b770
Emil Soman <emil.soman@gmail.com>
2015-09-23 17:22:13 +0530

    Fix Vault secret fetch, add more logs

--- lib/chatops_deployer/vault.rb
+++ lib/chatops_deployer/vault.rb
@@ -4,7 +4,7 @@ module ChatopsDeployer
   class Vault
     def read(secret, field)
       secret = ::Vault.logical.read(secret)
-      secret ? secret.data[field] : nil
+      secret ? secret.data[field.to_sym] : nil
     end
   end
 end
